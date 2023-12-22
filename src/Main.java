8944742268f8c85dd79758029ebd4594b08e3ec3
kiwirevol <sukhopara.nikita@yandex.ru>
2023-12-22 23:36:46 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Добрый день!");
+        int amountTicket = 1000;
+        System.out.println("Стоимость билета " + amountTicket + " руб.");
+        System.out.println("У нас работает бонусная программа, за каждые 20 рублей стоимости билета" +
+                " вам будет начисляться 1 бесплатная бонусная миля.");
+        int amountOneMiles = 20;
+        int bonusMiles = amountTicket / amountOneMiles;
+
+
+        System.out.println("Ваш бонус составляет " + bonusMiles + " миль.");
+
+
+    }
 }
